cefa71a25e499c9ac3106b71df34ae154482473c
Jonathan Stregger <32344150+JonathanStregger@users.noreply.github.com>
2020-09-20 11:38:58 -0600

    Update MountByZone.lua
    Add class mount by current class and liar zones

--- MountByZone.lua
+++ MountByZone.lua
@@ -1,9 +1,9 @@
 ----------------------------------------------------------------------
 -- 	Mount By Zone 0.0.01 (July 9, 2019)
+--  Author: Jonathan Stregger
 ----------------------------------------------------------------------
 
---local addonName, addon = ...
---local L = addon.L    
+
 function GetContinent()
     local mapID = C_Map.GetBestMapForUnit("player")
     local thisMapCont = { id=5, cont=""}
@@ -24,9 +24,8 @@ end
 local Ground = {"Kor'kron War Wolf", "Beastlord's Warwolf", "Ironside Warwolf", "Lionthien Prowler",
     "Scrapforged Mechaspider", "X-995 Mechanocat", "Warlord's Deathwheel", "Raven Lord", "Great Northern Elderhorn",
     "Highmountain Thunderhoof", "Spirit of Eche'ro", "Kor'kron Annihilator", "Core Hound"}
-local Flying = {"Huntmaster's Fierce Wolfhawk", "Huntmaster's Dire Wolfhawk", "Huntmaster's Loyal Wolfhawk",
-    "Infinite Timereaver", "Spawn of Galakras", "Dread Raven", "Grand Armored Wyvern"}
-local Swimming = {"Brinedeep Bottom-Feeder", "Crimson Tidestallion", "Sea Turtle"}
+local Flying = {"Spawn of Galakras", "Dread Raven", "Grand Armored Wyvern", "Obsidian Worldbreaker"}
+local Swimming = {"Brinedeep Bottom-Feeder", "Crimson Tidestallion", "Sea Turtle", "Darkwater Skate"}
 local GlobalMounts = {gnd = Ground,  fly = Flying, swim = Swimming}
 
 --BFA
@@ -49,8 +48,7 @@ local KulTiras = {name = "Kul Tiras", gnd = GroundBFA, fly = FlyBFA, swim = nil,
 --Legion
 local BrokenGnd = {"Lionthien Prowler", "Great Northern Elderhorn","Highmountain Thunderhoof", "Spirit of Eche'ro",
     "Nightbourne Manasaber"}
-local BrokenFly = {"Huntmaster's Fierce Wolfhawk", "Huntmaster's Dire Wolfhawk", "Huntmaster's Loyal Wolfhawk", 
-    "Leywoven Flying Carpet", "Vibrant Mana Ray", "Scintillating Mana Ray", "Felglow Mana Ray"}
+local BrokenFly = {"Leywoven Flying Carpet", "Vibrant Mana Ray", "Scintillating Mana Ray", "Felglow Mana Ray"}
 local GroundArgus = {"Amethyst Ruinstrider", "Beryl Ruinstrider", "Bleakhoof Ruinstrider", "Cerulean Ruinstrider", 
     "Russet Ruinstrider", "Sable Ruinstrider", "Umber Ruinstrider"}
 local Argus = {name = "Argus", gnd = GroundArgus, fly = nil, swim = nil}
@@ -88,11 +86,87 @@ local Continents = {Zandalar, KulTiras, BrokenIsles, Pandaria, Draenor, Northren
 
 local prevWeapons
 
+--Zones with mapType > 3 that are actually flyable
+local liarZones = {"Dazar'alor", "Dalaran"}
+
+--Detects a zone where the mapType > 3, but is flyable
+function IsLiarZone(zone)
+    for index, value in ipairs(liarZones) do
+        if value == zone then
+            return true
+        end
+    end
+    return false
+end
+
+
+--Add class mounts
+local class_name = UnitClass("player")
+if class_name == "Druid" then
+elseif class_name == "Hunter" then
+    local hunter_mounts = {"Huntmaster's Fierce Wolfhawk", "Huntmaster's Dire Wolfhawk", "Huntmaster's Loyal Wolfhawk"}
+    for i, continent in ipairs(Continents) do
+        if continent.fly then
+            for j, mount in ipairs(hunter_mounts) do
+                table.insert(Continents[i].fly, mount)
+            end
+        end
+    end
+elseif class_name == "Mage" then
+    for i, continent in ipairs(Continents) do
+        if continent.fly then table.insert(Continents[i].fly, "Archmage's Prismatic Disc") end
+    end
+elseif class_name == "Death Knight" then
+    for i, continent in ipairs(Continents) do
+        Continents[i].gnd = {"Acherus deathcharger"}
+        Continents[i].fly = {"Bloodbathed Frostbrood Vanquisher"}
+    end
+elseif class_name == "Rogue" then
+    for i, continent in ipairs(Continents) do
+        if continent.fly then table.insert(Continents[i].fly, "Shadowblade's Murderous Omen") end
+    end
+elseif class_name == "Warlock" then
+    for i, continent in ipairs(Continents) do
+        Continents[i].gnd = {"Netherlord's Chaotic Wrathsteed"}
+        Continents[i].fly = {"Netherlord's Chaotic Wrathsteed"}
+    end
+elseif class_name == "Demon Hunter" then
+    for i, continent in ipairs(Continents) do
+        Continents[i].gnd = {"Felsaber"}
+        Continents[i].fly = {"Slayer's Felbroken Shrieker"}
+    end
+elseif class_name == "Monk" then
+    for i, continent in ipairs(Continents) do
+        if continent.gnd then table.insert(Continents[i].gnd, "Ban-Lu, Grandmaster's Companion") end
+        if continent.fly then table.insert(Continents[i].fly, "Ban-Lu, Grandmaster's Companion") end
+    end
+elseif class_name == "Warrior" then
+    for i, continent in ipairs(Continents) do
+        if continent.fly then table.insert(Continents[i].fly, "Battlelord's Bloodthirsty War Wyrm") end
+    end
+elseif class_name == "Shaman" then
+    for i, continent in ipairs(Continents) do
+        if continent.fly then table.insert(Continents[i].fly, "Farseer's Raging Tempest") end
+    end
+elseif class_name == "Paladin" then
+    for i, continent in ipairs(Continents) do
+        Continents[i].gnd = {"Highlord's Golden Charger"}
+        Continents[i].fly = {"Highlord's Golden Charger"}
+    end
+elseif class_name == "Priest" then
+    for i, continent in ipairs(Continents) do
+        Continents[i].fly = {"High Priest's Lightsworm Seeker"}
+    end
+end
+
+
+--Gets the name of an appropriate mount
 function GetMount()
     local CurrentZone = GetZoneText()
     local CurrentCont = GetContinent()
-    --Check for instances first
-    if C_Map.GetMapInfo(C_Map.GetBestMapForUnit("PLAYER")).mapType > 3 then
+    local MType = C_Map.GetMapInfo(C_Map.GetBestMapForUnit("PLAYER")).mapType
+    
+    if MType > 3 and not IsLiarZone(CurrentZone) then
         mount = GlobalMounts.gnd[math.random(table.getn(GlobalMounts.gnd))]
     else
         if CurrentZone == "Nazjatar" or (IsSwimming() and not IsModifierKeyDown()) then
@@ -150,6 +224,8 @@ SlashCmdList["MOUNTBYZONE"] = function(msg)
             print(map.mapID .. " " .. map.name .. " " .. map.mapType .. " " .. map.parentMapID)
             map = C_Map.GetMapInfo(map.parentMapID)
         end
+    elseif class_name == "Druid" then
+        CastSpellByName("Travel Form")
     elseif not IsFlying() or (IsFlying() and IsModifierKeyDown()) then
         local mount = GetMount()
         if mount then
